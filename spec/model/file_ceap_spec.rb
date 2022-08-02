86b4977f7f32e245c74e872cd985c7bd40f95974
John Diego <johnoliveirasilva9@gmail.com>
2022-08-02 13:28:11 -0300

    update change attributes_for to build factory

--- spec/model/file_ceap_spec.rb
+++ spec/model/file_ceap_spec.rb
@@ -4,15 +4,15 @@ include ActionDispatch::TestProcess::FixtureFile
 RSpec.describe FileCeap, type: :model do
 
   let(:file_ceap) { create(:file_ceap, :with_attachment) }
-  let(:without_title) { attributes_for(:file_ceap, :with_attachment, title: nil)}
-  let(:without_attachement) { attributes_for(:file_ceap)}
+  let(:without_title) { build(:file_ceap, :with_attachment, title: nil)}
+  let(:without_attachement) { build(:file_ceap)}
   describe "valid model file_ceap" do
     it "is not valid without title" do
-      expect(described_class.new(without_title)).to_not be_valid
+      expect(without_title).to_not be_valid
     end
 
     it "is not valid without an attachment" do
-      expect(described_class.new(without_attachement)).to_not be_valid
+      expect(without_attachement).to_not be_valid
     end
 
     it 'is valid with an attachment' do
