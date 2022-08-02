7a8a5c2eec08e971d182c4c058c4c20019148d13
John Diego <johnoliveirasilva9@gmail.com>
2022-08-02 14:45:01 -0300

    create migrate expenditures (despesas)

---
+++ db/migrate/20220802171705_create_expenditure.rb
@@ -0,0 +1,14 @@
+class CreateExpenditure < ActiveRecord::Migration[6.0]
+  def change
+    create_table :expenditures do |t|
+      t.string :txt_fornecedor
+      t.string :cnpj_cpf
+      t.float  :vlr_liquido, precision: 5, scale: 2
+      t.date   :data_emissao
+      t.string :url_documento
+      t.string :descricao
+      t.belongs_to :deputy
+      t.timestamps
+    end
+  end
+end
