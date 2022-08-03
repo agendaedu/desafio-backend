869130429ac70f6e2ea6b019cf97d20d762e11ad
John Diego <johnoliveirasilva9@gmail.com>
2022-08-03 17:46:40 -0300

    test file

--- spec/controller/file_ceaps_controller_spec.rb
+++ spec/controller/file_ceaps_controller_spec.rb
@@ -6,16 +6,14 @@ RSpec.describe FileCeapsController, type: :controller do
 
   describe "POST #process_file" do
   let(:title) { "Ano-2021.csv" }
-    context "success" do
-      before do
-        post :process_file, params: {
-          name: title,
-          file: fixture_file_upload("spec/support/assets/files/Ano-2021.csv")
-        }, format: :json
-      end
-      it { expect(response).to have_http_status(:ok) }
-      # it { expect(FileCnab.count).to eq(1) }
-      # it { expect(FileCnab.last.title).to eq(title) }
+    before do
+      post :process_file, params: {
+        name: title,
+        file: fixture_file_upload("spec/support/assets/files/Ano-2021.csv")
+      }, format: :json
     end
+
+    it { expect(FileCeap.count).to eq(1) }
+    it { expect(FileCeap.last.title).to eq(title) }
   end
 end
