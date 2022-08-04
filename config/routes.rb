1cc8c852ebcee2c20a8beeb139f8dc95e19627da
John Diego <johnoliveirasilva9@gmail.com>
2022-08-04 17:24:14 -0300

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'home/index'
+  root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :homes
   resources :file_ceaps, only: [:index, :new, :create]
 end
