bfc739c6d85535e9a86af99ab2374c7c980fa881
John Diego <johnoliveirasilva9@gmail.com>
2022-08-04 17:59:58 -0300

    add callback

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -2,7 +2,12 @@ class Expenditure < ApplicationRecord
   validates :txt_fornecedor, :data_emissao, presence: true
   validates :vlr_liquido, :url_documento, presence: true
 
+  after_save :expenditure_maximum
   # Relationship
   belongs_to :deputy
   belongs_to :supplier
+
+  def expenditure_maximum
+    self.deputy.update_column(:maior_despesa, deputy.expenditures.maximum(:vlr_liquido))
+  end
 end
