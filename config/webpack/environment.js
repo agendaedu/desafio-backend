b266d1c741979176ac906eee455d1897c3a527fe
John Diego <johnoliveirasilva9@gmail.com>
2022-08-04 00:43:59 -0300

    add jquery to access modal

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,8 +3,8 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default']
   })
 )
