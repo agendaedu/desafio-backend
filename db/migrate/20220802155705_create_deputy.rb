b611f42651350afb0d56028dd48b7ccc98e4c256
John Diego <johnoliveirasilva9@gmail.com>
2022-08-05 13:52:34 -0300

    add field partido on deputy

--- db/migrate/20220802155705_create_deputy.rb
+++ db/migrate/20220802155705_create_deputy.rb
@@ -4,6 +4,7 @@ class CreateDeputy < ActiveRecord::Migration[6.0]
       t.string  :tx_nome_parlamentar
       t.integer :id_cadastro
       t.string  :cpf
+      t.string  :sg_partido
       t.string  :sg_uf
       t.timestamps
     end
