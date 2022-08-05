dcce25af86235fd7859ced17f97feabf9070f680
John Diego <johnoliveirasilva9@gmail.com>
2022-08-05 00:12:28 -0300

    add show graphics expenditures

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  before_action :search_depute, only: [:show]
   def index
     @q = Deputy.ransack(params[:q])
     @deputes = @q.result.includes(expenditures: :supplier)
@@ -6,5 +7,15 @@ class HomesController < ApplicationController
     respond_to do |format|
       format.html
     end
+  end
+
+  def show 
+    @expenditures = @depute.expenditures
+  end
+
+  private
+
+  def search_depute
+    @depute = Deputy.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :homes
+  resources :homes, only: [:index, :show]
   resources :file_ceaps, only: [:index, :new, :create]
 end
