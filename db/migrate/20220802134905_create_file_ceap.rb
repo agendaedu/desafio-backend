23eb7656a8112fe918387e57c309ba625241c2c3
John Diego <johnoliveirasilva9@gmail.com>
2022-08-05 13:54:53 -0300

    update column processed to status

--- db/migrate/20220802134905_create_file_ceap.rb
+++ db/migrate/20220802134905_create_file_ceap.rb
@@ -2,7 +2,7 @@ class CreateFileCeap < ActiveRecord::Migration[6.0]
   def change
     create_table :file_ceaps do |t|
       t.timestamps
-      t.boolean :processed, default: :false
+      t.integer :status, default: 0
       t.string :title
     end
   end
