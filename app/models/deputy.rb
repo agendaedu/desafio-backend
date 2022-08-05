ca0da9334f6ef2d5b337fa1d525327674ad45147
John Diego <johnoliveirasilva9@gmail.com>
2022-08-05 14:01:44 -0300

    add validates fields

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -2,7 +2,7 @@ class Deputy < ApplicationRecord
   validates :tx_nome_parlamentar, presence: true
   validates :sg_uf, presence: true
   validates :id_cadastro, presence: true, uniqueness: true
-  validates :cpf, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true
 
   # Relationship
   has_many :expenditures
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,4 @@
 class Supplier < ApplicationRecord
   validates :txt_fornecedor, presence: true
+  validates :txt_cnpj_cpf, presence: true
 end
