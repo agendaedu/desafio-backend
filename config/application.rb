b454d623832c00f2a110523bdcea200220c54259
John Diego <johnoliveirasilva9@gmail.com>
2022-08-06 07:10:34 -0300

    configure local translate

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module DesafioBackend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    I18n.default_locale = "pt-BR"
   end
 end
