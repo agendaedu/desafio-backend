45a30e7f1d506abb83fe1a4aa66d11da96938890
John Diego <johnoliveirasilva9@gmail.com>
2022-08-06 14:54:27 -0300

    add fontawesome and helper

--- app/models/file_ceap.rb
+++ app/models/file_ceap.rb
@@ -3,7 +3,11 @@ class FileCeap < ApplicationRecord
   validates :title, presence: true
   validates :avatar, presence: true
 
+  enum status: [:unprocessed, :in_process, :processed]
 
-  enum status: [ :processed, :unprocessed ]
-
+  def self.status_attributes_for_select
+    statuses.map do |status, _|
+      [I18n.t("activerecord.attributes.#{model_name.i18n_key}.status.#{status}"), status]
+    end
+  end
 end
