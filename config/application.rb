75f2b0ad7467c21405bfbc509f2a932cbcaadbbf
John Diego <johnoliveirasilva9@gmail.com>
2022-08-06 05:23:56 -0300

    configure sidekiq

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module DesafioBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :homes, only: [:index, :show]
