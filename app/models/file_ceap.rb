af7d27b4103a053f738f8234b5d6a74669537d1b
John Diego <johnoliveirasilva9@gmail.com>
2022-08-06 17:24:09 -0300

    validate type_document

--- app/models/file_ceap.rb
+++ app/models/file_ceap.rb
@@ -1,12 +1,20 @@
 class FileCeap < ApplicationRecord
   has_one_attached :avatar
   validates :title, :avatar, presence: true
-
+  validate :correct_document_mime_type
   enum status: [:unprocessed, :in_process, :processed]
 
   def self.status_attributes_for_select
     statuses.map do |status, _|
       [I18n.t("activerecord.attributes.#{model_name.i18n_key}.status.#{status}"), status]
+    end
+  end
+
+  private
+
+  def correct_document_mime_type
+    if avatar.attached? && !avatar.content_type.in?(%w(text/csv text/CSV))
+      errors.add(:avatar, t(".format_invalid"))
     end
   end
 end
