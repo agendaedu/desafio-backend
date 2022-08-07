ffb1e4b15064ef472734b56d6bdedfa512815478
John Diego <johnoliveirasilva9@gmail.com>
2022-08-06 22:29:57 -0300

    update env redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:7479/1') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:7479/1') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:7479/1') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:7479/1') }
 end
